588fff74f29b324068614b5434c90dec95d78d72
Julien.Rialland <julien.rialland@gmail.com>
2015-09-28 14:48:36 +0200

    use home dir instead of /tmp

--- javaformatter.sh
+++ javaformatter.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 set -e
-jarfile=/tmp/javaformatter-cli/target/javaformatter-cli-1.0-SNAPSHOT.jar
+tmpdir=$HOME/.javaformatter
+
+jarfile=$tmpdir/javaformatter-cli/target/javaformatter-cli-1.0-SNAPSHOT.jar
 
 which git > /dev/null
 which mvn > /dev/null
 which java > /dev/null
 
 if [ ! -f $jarfile ]; then
-    pushd /tmp >/dev/null
+    mkdir -p $tmpdir
+    pushd $tmpdir >/dev/null
     git clone https://github.com/jrialland/javaformatter-cli
     pushd javaformatter-cli >/dev/null
     mvn dependency:copy-dependencies package -DskipTests > /dev/null
     popd >/dev/null
     popd >/dev/null
 fi
-
 CLASSPATH=$jarfile
 for j in `find /tmp/javaformatter-cli/target/dependency -name "*.jar"`; do
     export CLASSPATH=$CLASSPATH:$j
 done
-
 java -cp "$CLASSPATH" com.github.jrialland.javaformatter.FormatterCli $@
+
