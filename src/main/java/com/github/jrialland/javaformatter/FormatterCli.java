629bbbdd6f4ec10c333af1132518ea37c9bc7bd7
Julien.Rialland <julien.rialland@gmail.com>
2015-09-28 14:06:02 +0200

    allow header text changes in source files

--- src/main/java/com/github/jrialland/javaformatter/FormatterCli.java
+++ src/main/java/com/github/jrialland/javaformatter/FormatterCli.java
@@ -19,120 +19,105 @@ import org.apache.commons.cli.Options;
 public class FormatterCli {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		Options opts = new Options();
-		
-		Option conf = Option.builder("c")
-			.longOpt("conf")
-			.required(false)
-			.numberOfArgs(1)
-			.desc("Eclipse configuration file to use")
-			.argName("url")
-			.build();
+
+		Option conf = Option.builder("c").longOpt("conf").required(false).numberOfArgs(1)
+				.desc("Eclipse configuration file to use").argName("url").build();
 		opts.addOption(conf);
-		
-		Option level = Option.builder("l")
-				.longOpt("level")
-				.required(false)
-				.numberOfArgs(1)
-				.argName("javaVersion")
-				.desc("source level")
-				.build();
+
+		Option level = Option.builder("l").longOpt("level").required(false).numberOfArgs(1).argName("javaVersion")
+				.desc("source level").build();
 		opts.addOption(level);
 
+		Option header = Option.builder("H").longOpt("header").required(false).numberOfArgs(1).argName("txtFile")
+				.desc("source file header").build();
+		opts.addOption(header);
 
-		Option encoding = Option.builder("e")
-				.longOpt("encoding")
-				.required(false)
-				.numberOfArgs(1)
-				.argName("charset")
-				.desc("source encoding")
-				.build();
+		Option encoding = Option.builder("e").longOpt("encoding").required(false).numberOfArgs(1).argName("charset")
+				.desc("source encoding").build();
 		opts.addOption(encoding);
-		
-		Option lsep = Option.builder("s")
-				.longOpt("linesep")
-				.required(false)
-				.hasArg(true)
-				.argName("charset")
-				.build();
-		opts.addOption(lsep);
-		
 
-		Option help = Option.builder("h")
-				.longOpt("help")
-				.hasArg(false)
-				.desc("Shows this help").build();
+		Option lsep = Option.builder("s").longOpt("linesep").required(false).hasArg(true).argName("charset").build();
+		opts.addOption(lsep);
+
+		Option help = Option.builder("h").longOpt("help").hasArg(false).desc("Shows this help").build();
 		opts.addOption(help);
-		
+
 		CommandLine cmd = new DefaultParser().parse(opts, args);
-		
-		if(cmd.hasOption("h")) {
+
+		if (cmd.hasOption("h")) {
 			HelpFormatter helpFormatter = new HelpFormatter();
 			helpFormatter.printHelp(FormatterCli.class.getSimpleName(), opts);
 			return;
 		}
-		
+
 		JavaFormatter formatter;
-		
-		if(cmd.hasOption("conf")) {
+
+		if (cmd.hasOption("conf")) {
 			formatter = new JavaFormatter(new URL(cmd.getOptionValue("conf")));
-		}
-		else {
+		} else {
 			formatter = new JavaFormatter();
 		}
-		
-		if(cmd.hasOption("level")) {
+
+		if (cmd.hasOption("level")) {
 			formatter.setSource(cmd.getOptionValue("level"));
 		}
-		
-		if(cmd.hasOption("encoding")) {
+
+		if (cmd.hasOption("encoding")) {
 			formatter.setEncoding(cmd.getOptionValue("encoding"));
 		}
-		
-		if(cmd.hasOption("linesep")) {
+
+		if (cmd.hasOption("linesep")) {
 			String linesep = cmd.getOptionValue("linesep");
-			if( ! Arrays.asList("lf", "cr", "crlf").contains(linesep)) {
+			if (!Arrays.asList("lf", "cr", "crlf").contains(linesep)) {
 				throw new IllegalArgumentException("linesep");
 			}
 			linesep = linesep.toLowerCase().replaceAll("cr", "\r").replaceAll("lf", "\n");
 			formatter.setLineSep(linesep);
 		}
-		
-		Path path = Paths.get(args[args.length-1]);
-		
-		if(Files.isRegularFile(path)) {
+
+		if (cmd.hasOption("header")) {
+			formatter.setHeader(Paths.get(cmd.getOptionValue("header")).toUri().toURL());
+		}
+
+		Path path = Paths.get(args[args.length - 1]);
+
+		if (Files.isRegularFile(path)) {
 			formatter.formatFile(path);
-		} else if(Files.isDirectory(path)) {
+		} else if (Files.isDirectory(path)) {
 			final JavaFormatter finalFormatter = formatter;
-			
+
 			Files.walkFileTree(path, new FileVisitor<Path>() {
 				@Override
 				public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
 					return FileVisitResult.CONTINUE;
 				}
+
 				@Override
 				public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
 					return FileVisitResult.CONTINUE;
 				}
+
 				@Override
 				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-					if(file.getFileName().toString().endsWith(".java")) {
+					if (file.getFileName().toString().endsWith(".java")) {
 						finalFormatter.formatFile(file);
-					} else if(file.getFileName().toString().endsWith(".xml")) {
+					} else if (file.getFileName().toString().endsWith(".xml")) {
 						new XmlFormatter().formatFile(file);
 					}
 					return FileVisitResult.CONTINUE;
 				}
+
 				@Override
 				public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
 					throw new RuntimeException("Error during formatting");
 				}
 			});
-			
+
 		} else {
-			throw new IllegalArgumentException("File or Directory not found : "+path);
+			throw new IllegalArgumentException("File or Directory not found : " + path);
 		}
-		
+
 	}
 }
