c4d23a7da7eda49ce51a670e16b4b5690310570a
Julien.Rialland <julien.rialland@gmail.com>
2015-09-29 13:47:46 +0200

    fixed js function name

--- src/main/java/com/github/jrialland/javaformatter/web/HtmlFormatter.java
+++ src/main/java/com/github/jrialland/javaformatter/web/HtmlFormatter.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 public class HtmlFormatter extends AbstractJsBeautifyFormatter{
 
 	public HtmlFormatter() {
-		super("bower_components/js-beautify/js/lib/beautify-html.js", "style_html","{'indent_inner_html': false,'indent_size': 2,'indent_char': ' ','wrap_line_length': 78,'brace_style': 'expand','unformatted': ['a', 'sub', 'sup', 'b', 'i', 'u'],'preserve_newlines': true,'max_preserve_newlines': 5,'indent_handlebars': false,'extra_liners': ['/html']}");
+		super("bower_components/js-beautify/js/lib/beautify-html.js", "html_beautify","{'indent_inner_html': false,'indent_size': 2,'indent_char': ' ','wrap_line_length': 78,'brace_style': 'expand','unformatted': ['a', 'sub', 'sup', 'b', 'i', 'u'],'preserve_newlines': true,'max_preserve_newlines': 5,'indent_handlebars': false,'extra_liners': ['/html']}");
 	}
 
 	@Override
