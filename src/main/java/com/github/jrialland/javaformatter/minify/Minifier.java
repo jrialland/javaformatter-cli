13f7dc15e557a8929a91e8ec2f479458609b8ec4
jrialland <julien.rialland@gmail.com>
2016-11-24 16:40:11 +0100

    yui does not work when rhino is installed

--- src/main/java/com/github/jrialland/javaformatter/minify/Minifier.java
+++ src/main/java/com/github/jrialland/javaformatter/minify/Minifier.java
@@ -1,3 +1,4 @@
+
 package com.github.jrialland.javaformatter.minify;
 
 import java.io.FileReader;
@@ -17,72 +18,72 @@ import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
 
 public class Minifier implements Transpiler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Minifier.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(Minifier.class);
 
-	@Override
-	public boolean accept(Path path) {
-		return Files.isRegularFile(path) && path.getFileName().toString().contains(".minify");
-	}
+  @Override
+  public boolean accept(Path path) {
+    return Files.isRegularFile(path) && path.getFileName().toString().contains(".minify");
+  }
 
-	@Override
-	public String getName() {
-		return "YUI compressor";
-	}
+  @Override
+  public String getName() {
+    return "YUI compressor";
+  }
 
-	@Override
-	public String getType() {
-		return "yui";
-	}
+  @Override
+  public String getType() {
+    return "yui";
+  }
 
-	private static ErrorReporter reporter = new ErrorReporter() {
+  private static ErrorReporter reporter = new ErrorReporter() {
 
-		@Override
-		public void warning(String message, String sourceName, int line, String lineSource, int lineOffset) {
-			if (line < 0) {
-				LOGGER.warn(message);
-			} else {
-				LOGGER.warn(line + ':' + lineOffset + ':' + message);
-			}
-		}
+    @Override
+    public void warning(String message, String sourceName, int line, String lineSource, int lineOffset) {
+      if (line < 0) {
+        LOGGER.warn(message);
+      } else {
+        LOGGER.warn(line + ':' + lineOffset + ':' + message);
+      }
+    }
 
-		@Override
-		public EvaluatorException runtimeError(String message, String sourceName, int line, String lineSource,
-				int lineOffset) {
-			error(message, sourceName, line, lineSource, lineOffset);
-			throw new RuntimeException(message);
-		}
+    @Override
+    public EvaluatorException runtimeError(String message, String sourceName, int line, String lineSource, int lineOffset) {
+      error(message, sourceName, line, lineSource, lineOffset);
+      throw new RuntimeException(message);
+    }
 
-		@Override
-		public void error(String message, String sourceName, int line, String lineSource, int lineOffset) {
-			if (line < 0) {
-				LOGGER.error(message);
-			} else {
-				LOGGER.error(line + ':' + lineOffset + ':' + message);
-			}
-		}
-	};
+    @Override
+    public void error(String message, String sourceName, int line, String lineSource, int lineOffset) {
+      if (line < 0) {
+        LOGGER.error(message);
+      } else {
+        LOGGER.error(line + ':' + lineOffset + ':' + message);
+      }
+    }
+  };
 
-	@Override
-	public void transpile(Path file) {
-		try {
-			Path output = Paths.get(file.toAbsolutePath().toString().replaceFirst("\\.minify", ".min"));
-			FileWriter out = new FileWriter(output.toFile());
-			if (file.toString().endsWith(".css")) {
-				CssCompressor cssCompressor = new CssCompressor(new FileReader(file.toFile()));
-				cssCompressor.compress(out, 0);
-			} else if (file.toString().endsWith(".js")) {
-				JavaScriptCompressor jsCompressor = new JavaScriptCompressor(new FileReader(file.toFile()), reporter);
-				jsCompressor.compress(out, 0, true, false, false, false);
-			}
-			out.flush();
-			out.close();
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	@Override
-	public String getShortdesc() {
-	  return "minifies css and js files. Applies on files named *.css.minify or *.js.minify";
-	}
+  @Override
+  public void transpile(Path file) {
+    try {
+      Path output = Paths.get(file.toAbsolutePath().toString().replaceFirst("\\.minify", ".min"));
+      FileWriter out = new FileWriter(output.toFile());
+      if (file.toString().endsWith(".css")) {
+        CssCompressor cssCompressor = new CssCompressor(new FileReader(file.toFile()));
+        cssCompressor.compress(out, 0);
+      } else if (file.toString().endsWith(".js")) {
+        JavaScriptCompressor jsCompressor = new JavaScriptCompressor(new FileReader(file.toFile()), reporter);
+        jsCompressor.compress(out, 0, true, false, false, false);
+      }
+      out.flush();
+      out.close();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public String getShortdesc() {
+    return "minifies css and js files. Applies on files named *.css.minify or *.js.minify";
+  }
+
 }
